010cea56d3219bbbfc2c1d8cc6344327c4d5746b
Luana Faria Lane <luanasfff@gmail.com>
2020-01-05 03:24:48 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,30 @@
 class Cat
-  # code goes here
+
+  attr_accessor :owner, :mood 
+  attr_reader   :name 
+
+   @@all = []
+   
+   # INSTANCE METHODS  
+   
+   
+  def initialize(name, owner)     #INITIALIZES W/ NAME AND AN OWNER 
+    @name = name 
+    @owner = owner         
+    @mood = "nervous"             #INITIALIZES W/ A NERVOUS MOOD 
+    @@all << self 
+  end 
+
+
+
+  
+
+
+   #CLASS METHODS  ( KNOWS ALL THE CATS)
+  def self.all 
+    @@all
+  end  
+  
+  
 end
+
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,31 @@
-class Dog
-  # code goes here
+ class Dog 
+ 
+ 
+   attr_accessor :owner, :mood 
+   attr_reader :name 
+   
+      @@all = [] 
+  
+  
+  
+    
+    #NTANCE METHODS
+    
+    
+    def initialize(name, owner)   
+      @name = name 
+      @mood = "nervous" 
+      @owner = owner 
+      @@all << self 
+    end
+
+    
+    
+    #CLASS METHODS  ( Knows all the dogs)
+
+    def self.all 
+      @@all
+    end 
+
 end
+
