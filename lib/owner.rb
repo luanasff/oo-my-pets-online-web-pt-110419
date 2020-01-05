0f78337dd2784101e8f9be3bbdc1b1b7392f4108
Luana Faria Lane <luanasfff@gmail.com>
2020-01-05 02:52:40 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -75,7 +75,7 @@ class Owner
     
     
     def list_pets
-      "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)." 
+      p "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)." 
     end 
     
     
