df6cd3e963add645fd5114d520b8b62ce77fa2ca
Luana Faria Lane <luanasfff@gmail.com>
2020-01-05 02:40:27 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -36,7 +36,13 @@ class Owner
        
      end 
     
-    
+    def cats 
+      Cat.all.select {|cat| cat.owner == self}  
+    end 
+
+    def dogs  
+      Dog.all.select {|dog| dog.owner == self}  
+    end 
     
     
     
